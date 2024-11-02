64459af6b50c267f0c4028f53c823aba7445dd8a
Abhinn-Agrawal <abhinnagrawal18@gmail.com>
2024-11-02 18:27:44 +0530

    Color coding

--- app/src/main/java/com/example/chattingapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/chattingapp/feature/home/HomeScreen.kt
@@ -1,22 +1,30 @@
 package com.example.chattingapp.feature.home
 
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -26,12 +34,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.chattingapp.ui.theme.Channels
+import com.example.chattingapp.ui.theme.DarkGray
+import com.example.chattingapp.ui.theme.LightBlue
 import com.example.chattingapp.ui.theme.Text2
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,6 +56,7 @@ fun HomeScreen(navController: NavController){
     val channels = viewModel.channels.collectAsState()
     val addChannel = remember{ mutableStateOf(false) }
     val sheetState = rememberModalBottomSheetState()
+
     Scaffold(
         floatingActionButton = {
             Box(modifier = Modifier
@@ -57,26 +72,32 @@ fun HomeScreen(navController: NavController){
                     color = Color.White
                 )
             }
-        }
+        },
+        containerColor = Color.Black
     ) {
         Box(modifier = Modifier
             .padding(it)
             .fillMaxSize()
         ){
             LazyColumn {
+                item{
+                    Text2("Messages", color = Color.Gray,fontSize = 40.sp, fontWeight = FontWeight.Black, modifier = Modifier.padding(16.dp))
+                }
+                item{
+                    TextField(value = "", onValueChange = {},
+                        placeholder = { Text2(text = "Search...")},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                            .clip(RoundedCornerShape(40.dp)),
+                        textStyle = TextStyle(Color.LightGray),
+                        colors = TextFieldDefaults.colors().copy(focusedContainerColor = DarkGray, unfocusedContainerColor = DarkGray),
+                        leadingIcon = {Icon(imageVector = Icons.Filled.Search, contentDescription = null)}
+                    )
+                }
                 items(channels.value){channel->
                     Column {
-                        Text2(text = channel.name,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(8.dp)
-                                .clip(RoundedCornerShape(16.dp))
-                                .background(Color.Red.copy(0.3f))
-                                .clickable {
-                                    navController.navigate("chat/${channel.id}")
-                                }
-                                .padding(16.dp)
-                        )
+                        ChannelItem(channelName = channel.name, onClick = {navController.navigate("chat/${channel.id}")})
                     }
                 }
             }
@@ -94,9 +115,39 @@ fun HomeScreen(navController: NavController){
 }
 
 @Composable
+fun ChannelItem(channelName:String , onClick:() -> Unit){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp , vertical = 2.dp)
+        .clip(RoundedCornerShape(16.dp))
+        .background(DarkGray)
+        .clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(8.dp)
+                .size(70.dp)
+                .clip(CircleShape)
+                .background(LightBlue.copy(alpha = 0.3f))
+        ) {
+            Text2(
+                text = channelName[0].uppercase(),
+                fontSize = 35.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+        Text2(text = channelName, modifier = Modifier.padding(8.dp), color = Color.White)
+    }
+}
+
+@Composable
 fun AddChannelDialog(onAddChannel:(String) -> Unit){
     val channelName = remember{ mutableStateOf("") }
-    Column (modifier = Modifier.padding(16.dp)
+    Column (modifier = Modifier
+        .padding(8.dp)
         .fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
